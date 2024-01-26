419b5fc35e750189413784c551dc0ec7732526c7
Thevihn5804 <thevinh58.o4@gmail.com>
2024-01-26 10:13:40 +0700

    nhapthangtrongnam

---
+++ lab 2/bai 4 lab 2/nhapthangtrongnam/src/thangtrongnam.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+/**
+     * nhapsonguyentu1-12
+     */
+    public class thangtrongnam {
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("nhap n");
+            int n=sc.nextInt();
+            switch (n) {
+                case 1:
+                    System.out.println("Thang 1");
+                    break;
+                case 2 :
+                    System.out.println("Thang 2");
+                    break;
+                case 3 :
+                    System.out.println("Thang 3");
+                    break;
+                case 4 :
+                    System.out.println("Thang 4");
+                    break;
+                case 5 :
+                    System.out.println("Thang 5");
+                    break;
+                case 6 :
+                    System.out.println("Thang 6");
+                    break;
+                case 7 :
+                    System.out.println("Thang 7");
+                    break;
+                case 8 :
+                    System.out.println("Thang 8");
+                    break;
+                case 9 :
+                    System.out.println("Thang 9");
+                    break;
+                case 10 :
+                    System.out.println("Thang 10");
+                    break;
+                case 11 :
+                    System.out.println("Thang 11");
+                    break;
+                case 12 :
+                    System.out.println("Thang 12");
+                    break;
+                    default :
+                    System.out.println("KHong hop le");                        
+            }
+            sc.close();
+        }
+
+    }
