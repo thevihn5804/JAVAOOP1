aebdabb77a0cb0e0589d471b24b9c2b8bbea3302
Thevihn5804 <thevinh58.o4@gmail.com>
2024-01-31 15:47:58 +0700

    Slide55chuong2

---
+++ baithuchanh/src/Slide55chuong2.java/Slide55chuong2/src/Slide55chuong2.java
@@ -0,0 +1,13 @@
+/**
+ * Slide55chuong2
+ */
+public class Slide55chuong2 {
+
+    public static void main(String[] args) {
+        System.out.println("Cac so chia het cho 5 la: ");
+        for(int i = 0; i<=20; i++)
+            if(i % 5 == 0){
+            System.out.println(i);
+            }       
+    }
+}
