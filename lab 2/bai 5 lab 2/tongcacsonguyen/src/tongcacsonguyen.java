6cf7ea35f99fa2c54bf5c3964b3d9e4f322208c7
Thevihn5804 <thevinh58.o4@gmail.com>
2024-01-31 15:30:11 +0700

    bai 5 lab 2

---
+++ lab 2/bai 5 lab 2/tongcacsonguyen/src/tongcacsonguyen.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+     * tongcacsonguyen
+     */
+    public class tongcacsonguyen {
+    
+        public static void main(String[] args) {
+            int n,sum = 0;
+            String strResoult = "";
+            Scanner sc= new Scanner(System.in);
+            n = 0;
+            while (sum <=100) {
+                System.out.println("nhap cac so: ");
+                n = sc.nextInt();
+                sum += n;
+                strResoult = strResoult + n + " + ";
+            }
+            strResoult= strResoult.substring(0,strResoult.length()-2);
+            System.out.print(strResoult + "= " +  sum + " ");
+            sc.close();
+        }
+    }
