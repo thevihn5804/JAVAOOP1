a0605decb967370d14b4b285d362a4a8a4211d10
Thevihn5804 <thevinh58.o4@gmail.com>
2024-02-07 20:46:38 +0700

    bai6lab2

---
+++ lab 2/bai 6 lab 2/bai6lab2/src/bai6lab2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class bai6lab2 {
+    public static void main(String[] args) {
+        int n, i=1;
+        long  giaithua = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap so: ");
+        n = sc.nextInt();
+        do{   
+            giaithua *=i;
+            i++;
+        }while(i<=n);
+        System.out.print("giai thua cua " + n + " bang " + giaithua  );
+        sc.close();
+    }
+    
+}
