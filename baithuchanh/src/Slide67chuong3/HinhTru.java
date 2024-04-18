c354c290f56e49b4f1a93dfc54c73d08bff001de
Thevihn5804 <thevinh58.o4@gmail.com>
2024-04-18 23:58:06 +0700

    slide67chuong3

---
+++ baithuchanh/src/Slide67chuong3/HinhTru.java
@@ -0,0 +1,7 @@
+package Slide67chuong3;
+
+public class HinhTru extends HinhTron {
+    public static void xuatThongTin() {
+        System.out.println("Day la hinh tru");
+    }
+}
---
+++ baithuchanh/src/Slide67chuong3/mainClass.java
@@ -0,0 +1,8 @@
+package Slide67chuong3;
+
+public class mainClass {
+    public static void main(String[] args) {
+        HinhTron.xuatThongTin();
+        HinhTru.xuatThongTin();
+    }
+}
