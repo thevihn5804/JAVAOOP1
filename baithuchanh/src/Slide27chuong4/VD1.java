04bd8c5dcb5503d8c328f9dde8e7a2fa538f23de
Thevihn5804 <thevinh58.o4@gmail.com>
2024-05-09 21:14:33 +0700

    Slide27chuong4

---
+++ baithuchanh/src/Slide27chuong4/VD1.java
@@ -0,0 +1,9 @@
+package Slide27chuong4;
+
+import java.util.LinkedList;
+
+public class VD1 {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+    }
+}
---
+++ baithuchanh/src/Slide27chuong4/VD2.java
@@ -0,0 +1,9 @@
+package Slide27chuong4;
+
+import java.util.LinkedList;
+
+public class VD2 {
+    public static void main(String[] args) {
+         LinkedList list = new LinkedList<>();
+    }
+}
